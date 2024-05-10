a40e06807c291407ba04d988a0230321d5df6fae
avd <aravind19112003@gmail.com>
2024-05-10 11:39:10 +0530

    Add JWT Auth

--- services/products.py
+++ services/products.py
@@ -1,9 +1,30 @@
 import requests
+from flask import Flask, jsonify, request, make_response
+import jwt
+from functools import wraps
+import json
 import os
-from flask import Flask, jsonify
+from jwt.exceptions import DecodeError
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
 port = int(os.environ.get('PORT', 5000))
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.cookies.get('token')
+        if not token:
+            return jsonify({'error': 'Authorization token is missing'}), 401
+        try:
+            data = jwt.decode(
+                token, app.config['SECRET_KEY'], algorithms=["HS256"])
+            current_user_id = data['user_id']
+        except DecodeError:
+            return jsonify({'error': 'Authorization token is invalid'}), 401
+        return f(current_user_id, *args, **kwargs)
+    return decorated
 
 
 @app.route("/")
@@ -31,6 +52,27 @@ def get_products():
         }
         products.append(product_data)
     return jsonify({'data': products}), 200 if products else 204
+
+
+with open('users.json', 'r') as f:
+    users = json.load(f)
+
+
+@app.route('/auth', methods=['POST'])
+def authenticate_user():
+    if request.headers['Content-Type'] != 'application/json':
+        return jsonify({'error': 'Unsupported Media Type'}), 415
+    username = request.json.get('username')
+    password = request.json.get('password')
+    for user in users:
+        if user['username'] == username and user['password'] == password:
+            token = jwt.encode(
+                {'user_id': user['id']}, app.config['SECRET_KEY'], algorithm="HS256")
+            response = make_response(
+                jsonify({'message': 'Authentication successful'}))
+            response.set_cookie('token', token)
+            return response, 200
+    return jsonify({'error': 'Invalid username or password'}), 401
 
 
 if __name__ == "__main__":
